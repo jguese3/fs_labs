b793b47b7ee0a7a164abf4894b9bc8d7d1714b9a
Jarone Guese <jguese3@academic.rrc.ca>
2026-06-16 18:39:56 -0500

    reverted organization service

--- src/services/organizationService.ts
+++ src/services/organizationService.ts
@@ -1,4 +1,4 @@
-import * as organizationRepo from "../repository/organizationRepo";
+import { organizationRepo } from "../repository/organizationRepo";
 
 export const organizationService = {
 
@@ -9,11 +9,39 @@ export const organizationService = {
     ) {
 
         const firstNameErrors: string[] = [];
+        const roleErrors: string[] = [];
 
+        // Rule 1
         if (firstName.trim().length < 3) {
             firstNameErrors.push(
                 "First name must be at least 3 characters."
             );
+        }
+
+        const role =
+            organizationRepo
+                .getRoles()
+                .find(r => r.title === roleTitle);
+
+        // Rule 2
+        if (!role) {
+            roleErrors.push("Role does not exist.");
+        }
+
+        // Rule 3
+        else if (role.employee) {
+            roleErrors.push("Role is already occupied.");
+        }
+
+        if (
+            firstNameErrors.length ||
+            roleErrors.length
+        ) {
+            return {
+                success: false,
+                firstNameErrors,
+                roleErrors
+            };
         }
 
         const updated =
@@ -27,6 +55,7 @@ export const organizationService = {
             success: true,
             role: updated,
             firstNameErrors: [],
+            roleErrors: []
         };
     }
 };
